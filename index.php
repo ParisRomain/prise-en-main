17522cf936715bec55a083886b7fcbc95af6c334
oxsparks <paris.romain.pro@gmail.com>
2017-01-10 11:32:22 +0100

    base de l'index.php
    base html

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mon site avec GitHub</title>
+  </head>
+  <body>
+    <header>
+      <h1>Mon Site GitHub</h1>
+    </header>
+  </body>
+</html>
